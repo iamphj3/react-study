892070626373fd5d6eec65f70f08fb54babbfee0
parkhyeonji <iamphj3@naver.com>
2022-09-08 17:07:37 +0900

    #7.5 React Router

--- react-for-beginners/src/Movie.js
+++ react-for-beginners/src/Movie.js
@@ -1,4 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
 function Movie() {
-    return null;
+    return (<Router>
+        {/* 한 번에 하나의 route만 렌더링하기 위해 Routes 사용 (이전 버전 : switch) */}
+        <Routes>
+            <Route path="/movie" element={<Detail />}>
+            </Route>
+            <Route path="/" element={<Home />}>
+            </Route>
+        </Routes>
+    </Router>
+    );
 }
 export default Movie;
